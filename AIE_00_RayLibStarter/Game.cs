cae4cfa7076120bd61210328aaae24f3c8cc5af9
John Millard <jtm6667@gmail.com>
2022-02-08 16:28:18 +1030

    Added Raylib starter project and solution folders to group projects

---
+++ AIE_00_RayLibStarter/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Raylib_cs;
+
+namespace AIE_00_RayLibStarter
+{
+    class Game
+    {
+        public int windowWidth = 500;
+        public int windowHeight = 500;
+        public string windowTitle = "Your Project Name";
+
+        public void Load()
+        {
+            // TODO:
+            // load game assets here
+        }
+
+        public void Update()
+        {
+            // TODO:
+            // Add game update logic here - this code will run 60 times per second
+        }
+        
+        public void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RAYWHITE);
+            
+            // TODO:
+            // Add game drawing logic here - this code will run 60 times per second
+            // ​
+            Raylib.EndDrawing();
+        }
+    }
+}
---
+++ AIE_00_RayLibStarter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Raylib_cs;
+
+namespace AIE_00_RayLibStarter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            
+            Raylib.InitWindow(game.windowWidth, game.windowHeight, game.windowTitle);
+            Raylib.SetTargetFPS(60);
+            
+            game.Load();
+            
+            while (!Raylib.WindowShouldClose())
+            {
+                game.Update();
+                game.Draw();
+            }
+            
+            Raylib.CloseWindow();
+        }
+    }
+}
